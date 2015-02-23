ae98e18cfd548e7167424a0a9d42710fe49e8fc1
Denis Stepulenok <super.denis@gmail.com>
2015-02-23 15:47:20 +0300

    Save changes

--- lesson_01/00_first/p.py
+++ lesson_01/00_first/p.py
@@ -1,8 +1,25 @@
-with open("helloworld.c") as f:
+import sys
+
+PREFIX = u'//*'
+
+sys.stdout = open("README.md", "w", encoding="utf-8")
+
+code = False
+with open("helloworld.c", "r", encoding="utf-8-sig") as f:
     for line in f:
         if '//-->' in line:
-          print('``` cpp')
-        print(line)
+            print('``` cpp')
+            code = True
+            continue
         if '//<--' in line:
-          print('```')
+            print('```')
+            code = False
+            continue
+        if code:
+            print(line.rstrip())
+            continue
+        if line.startswith(PREFIX):
+            print(line[len(PREFIX):].strip())
+
+
 
