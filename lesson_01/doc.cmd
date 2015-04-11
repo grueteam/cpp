f277bb1ed25ed199080326b6511e9b2c3e5c5094
Denis Stepulenok <super.denis@gmail.com>
2015-04-11 11:43:31 +0300

    Save changes

--- lesson_01/doc.cmd
+++ lesson_01/doc.cmd
@@ -1,9 +1,9 @@
-SET PATH=C:\Python34;%PATH%
-python.exe doc.py
-
 "..\AStyle" --options=..\astyle.cfg --recursive *.c 
 "..\AStyle" --options=..\astyle.cfg --recursive *.cpp 
 "..\AStyle" --options=..\astyle.cfg --recursive *.h 
+
+SET PATH=C:\Python34;%PATH%
+python.exe doc.py
 
 git add .
 git commit -a -m"Save changes"
